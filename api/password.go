b7e01851d5d04fafe1e30df8e721767934b16941
aaleksandrov <aladex@gmail.com>
2020-04-16 19:14:49 +0300

    refactoring and documentation

--- api/password.go
+++ api/password.go
@@ -14,6 +14,10 @@ import (
 	"net/http"
 )
 
+type changeAnswer struct {
+	Message string `json:"message"`
+}
+
 type passwordToken struct {
 	Token    string `json:"token" form:"token" bindings:"required"`
 	Password string `json:"password" form:"password"`
@@ -23,6 +27,19 @@ type passwordChangeRequest struct {
 	Email string `json:"email" form:"email" bindings:"required"`
 }
 
+// TokenValidation check for token validation
+// Auth godoc
+// @Summary check for token validation
+// @Description check for token validation
+// @Tags login
+// @Accept  json
+// @Produce  json
+// @Param  body body passwordToken true "Token Info"
+// @Success 200 {object} string
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 403 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /token [post]
 func TokenValidation(c *gin.Context) {
 	var token passwordToken
 	if err := c.ShouldBindJSON(&token); err == nil {
@@ -37,6 +54,19 @@ func TokenValidation(c *gin.Context) {
 	httputil.NewError(c, http.StatusBadRequest, errors.New("bad_request"))
 }
 
+// ChangeRequest method for change request
+// Auth godoc
+// @Summary method for change request
+// @Description method for change request
+// @Tags login
+// @Accept  json
+// @Produce  json
+// @Param  body body passwordChangeRequest true "user info"
+// @Success 200 {object} changeAnswer
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 403 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /change-request [post]
 func ChangeRequest(c *gin.Context) {
 	var changeRequest passwordChangeRequest
 	if err := c.ShouldBindJSON(&changeRequest); err == nil {
@@ -67,12 +97,27 @@ func ChangeRequest(c *gin.Context) {
 			}
 		}()
 
-		c.JSON(200, "token_created")
+		c.JSON(200, changeAnswer{
+			"token_created",
+		})
 		return
 	}
 	httputil.NewError(c, http.StatusBadRequest, errors.New("bad_request"))
 }
 
+// ChangeUserState method for change user
+// Auth godoc
+// @Summary method for change user
+// @Description method for change user
+// @Tags login
+// @Accept  json
+// @Produce  json
+// @Param  body body passwordToken true "user info"
+// @Success 200 {object} models.LoggedInUser
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 403 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /change-password [post]
 func ChangeUserState(c *gin.Context) {
 	var token passwordToken
 	if err := c.ShouldBindJSON(&token); err == nil {
