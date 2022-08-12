ea667a1df8a0c2b877bd66ffd9819f3afce025e6
Andrei Aleksandrov <aaleksandrov@cinemo.com>
2022-08-12 15:24:12 +0200

    start page generation

--- api/bot_api.go
+++ api/bot_api.go
@@ -1,128 +1,15 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"gopds-api/database"
 	"gopds-api/httputil"
 	"gopds-api/logging"
 	"gopds-api/models"
+	"gopds-api/telegram"
 	"net/http"
 )
-
-// NewInlineKeyboardRow creates an inline keyboard row with buttons.
-func NewInlineKeyboardRow(buttons ...InlineKeyboardButton) []InlineKeyboardButton {
-	var row []InlineKeyboardButton
-
-	row = append(row, buttons...)
-
-	return row
-}
-
-// NewInlineKeyboardMarkup creates a new inline keyboard.
-func NewInlineKeyboardMarkup(rows ...[]InlineKeyboardButton) InlineKeyboardMarkup {
-	var keyboard [][]InlineKeyboardButton
-
-	keyboard = append(keyboard, rows...)
-
-	return InlineKeyboardMarkup{
-		InlineKeyboard: keyboard,
-	}
-}
-
-// NewInlineKeyboardButtonData creates an inline keyboard button with text
-// and data for a callback.
-func NewInlineKeyboardButtonData(text, data string) InlineKeyboardButton {
-	return InlineKeyboardButton{
-		Text:         text,
-		CallbackData: &data,
-	}
-}
-
-// InlineKeyboardButton represents one button of an inline keyboard. You must
-// use exactly one of the optional fields.
-//
-// Note that some values are references as even an empty string
-// will change behavior.
-//
-// CallbackGame, if set, MUST be first button in first row.
-type InlineKeyboardButton struct {
-	// Text label text on the button
-	Text         string  `json:"text"`
-	CallbackData *string `json:"callback_data,omitempty"`
-}
-
-// InlineKeyboardMarkup represents an inline keyboard that appears right next to
-// the message it belongs to.
-type InlineKeyboardMarkup struct {
-	// InlineKeyboard array of button rows, each represented by an Array of
-	// InlineKeyboardButton objects
-	InlineKeyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
-}
-
-type BaseChat struct {
-	ChatID int64 `json:"chat_id"` // required
-	//ChannelUsername          string `json:"channel_username"`
-	//ProtectContent           bool `json:"protect_content"`
-	//ReplyToMessageID         int `json:"reply_to_message_id"`
-	ReplyMarkup interface{} `json:"reply_markup"`
-	//DisableNotification      bool `json:"disable_notification"`
-	//AllowSendingWithoutReply bool `json:"allow_sending_without_reply"`
-	Text string `json:"text"`
-}
-
-// MessageConfig Message represents a message.
-type MessageConfig struct {
-	BaseChat              `json:"base_chat"`
-	Text                  string `json:"text"`
-	ParseMode             string `json:"parse_mode"`
-	DisableWebPagePreview bool   `json:"disable_web_page_preview"`
-}
-
-type TelegramCommand struct {
-	UpdateID int `json:"update_id"`
-	Message  struct {
-		Date int `json:"date"`
-		Chat struct {
-			LastName  string `json:"last_name"`
-			ID        int    `json:"id"`
-			Type      string `json:"type"`
-			FirstName string `json:"first_name"`
-			Username  string `json:"username"`
-		} `json:"chat"`
-		MessageID int `json:"message_id"`
-		From      struct {
-			LastName  string `json:"last_name"`
-			ID        int    `json:"id"`
-			FirstName string `json:"first_name"`
-			Username  string `json:"username"`
-		} `json:"from"`
-		Text string `json:"text"`
-	} `json:"message"`
-}
-
-func SendCommand(token string, m BaseChat) {
-	b, err := json.Marshal(m)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(b))
-	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", token)
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-}
 
 func TokenApiEndpoint(c *gin.Context) {
 	botToken := c.Param("id")
@@ -131,7 +18,7 @@ func TokenApiEndpoint(c *gin.Context) {
 		httputil.NewError(c, http.StatusNotFound, errors.New("user_is_not_found"))
 		return
 	}
-	var telegramCmd TelegramCommand
+	var telegramCmd telegram.TelegramCommand
 
 	if err := c.ShouldBindJSON(&telegramCmd); err == nil {
 		if user.TelegramID == 0 {
@@ -146,24 +33,25 @@ func TokenApiEndpoint(c *gin.Context) {
 			}
 		}
 	}
-
-	var numericKeyboard = NewInlineKeyboardMarkup(
-		NewInlineKeyboardRow(
-			NewInlineKeyboardButtonData("2", "2"),
-			NewInlineKeyboardButtonData("3", "3"),
-		),
-		NewInlineKeyboardRow(
-			NewInlineKeyboardButtonData("4", "4"),
-			NewInlineKeyboardButtonData("5", "5"),
-			NewInlineKeyboardButtonData("6", "6"),
-		),
-	)
-	m := BaseChat{
-		ChatID: int64(user.TelegramID),
-		//ReplyToMessageID: 0,
-		Text:        "test",
-		ReplyMarkup: numericKeyboard,
+	tgMessage := telegram.NewBaseChat(int64(user.TelegramID), "")
+	if telegramCmd.Message.Text == "/start" {
+		tgMessage, err = telegram.TelegramBooksList(user, models.BookFilters{
+			Limit:      5,
+			Offset:     0,
+			Title:      "",
+			Author:     0,
+			Series:     0,
+			Lang:       "",
+			Fav:        false,
+			UnApproved: false,
+		})
+		if err != nil {
+			logging.CustomLog.Println(err)
+			httputil.NewError(c, http.StatusBadRequest, errors.New("bad request"))
+			return
+		}
 	}
-	go SendCommand(user.BotToken, m)
+
+	go telegram.SendCommand(user.BotToken, tgMessage)
 	c.JSON(200, "Ok")
 }
---
+++ telegram/bookslist.go
@@ -0,0 +1,162 @@
+package telegram
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"gopds-api/database"
+	"gopds-api/models"
+	"net/http"
+	"strings"
+)
+
+func NewBaseChat(chatID int64, text string) BaseChat {
+	return BaseChat{
+		ChatID:    chatID,
+		Text:      text,
+		ParseMode: "HTML",
+	}
+}
+
+// NewInlineKeyboardRow creates an inline keyboard row with buttons.
+func NewInlineKeyboardRow(buttons ...InlineKeyboardButton) []InlineKeyboardButton {
+	var row []InlineKeyboardButton
+
+	row = append(row, buttons...)
+
+	return row
+}
+
+// NewInlineKeyboardMarkup creates a new inline keyboard.
+func NewInlineKeyboardMarkup(rows ...[]InlineKeyboardButton) InlineKeyboardMarkup {
+	var keyboard [][]InlineKeyboardButton
+
+	keyboard = append(keyboard, rows...)
+
+	return InlineKeyboardMarkup{
+		InlineKeyboard: keyboard,
+	}
+}
+
+// NewInlineKeyboardButtonData creates an inline keyboard button with text
+// and data for a callback.
+func NewInlineKeyboardButtonData(text, data string) InlineKeyboardButton {
+	return InlineKeyboardButton{
+		Text:         text,
+		CallbackData: &data,
+	}
+}
+
+// InlineKeyboardButton represents one button of an inline keyboard. You must
+// use exactly one of the optional fields.
+//
+// Note that some values are references as even an empty string
+// will change behavior.
+//
+// CallbackGame, if set, MUST be first button in first row.
+type InlineKeyboardButton struct {
+	// Text label text on the button
+	Text         string  `json:"text"`
+	CallbackData *string `json:"callback_data,omitempty"`
+}
+
+// InlineKeyboardMarkup represents an inline keyboard that appears right next to
+// the message it belongs to.
+type InlineKeyboardMarkup struct {
+	// InlineKeyboard array of button rows, each represented by an Array of
+	// InlineKeyboardButton objects
+	InlineKeyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
+}
+
+type BaseChat struct {
+	ChatID int64 `json:"chat_id"` // required
+	//ChannelUsername          string `json:"channel_username"`
+	//ProtectContent           bool `json:"protect_content"`
+	//ReplyToMessageID         int `json:"reply_to_message_id"`
+	ReplyMarkup interface{} `json:"reply_markup"`
+	//DisableNotification      bool `json:"disable_notification"`
+	//AllowSendingWithoutReply bool `json:"allow_sending_without_reply"`
+	Text      string `json:"text"`
+	ParseMode string `json:"parse_mode"`
+}
+
+// MessageConfig Message represents a message.
+type MessageConfig struct {
+	BaseChat              `json:"base_chat"`
+	Text                  string `json:"text"`
+	ParseMode             string `json:"parse_mode"`
+	DisableWebPagePreview bool   `json:"disable_web_page_preview"`
+}
+
+type TelegramCommand struct {
+	UpdateID int `json:"update_id"`
+	Message  struct {
+		Date int `json:"date"`
+		Chat struct {
+			LastName  string `json:"last_name"`
+			ID        int    `json:"id"`
+			Type      string `json:"type"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+		} `json:"chat"`
+		MessageID int `json:"message_id"`
+		From      struct {
+			LastName  string `json:"last_name"`
+			ID        int    `json:"id"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+		} `json:"from"`
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+func SendCommand(token string, m BaseChat) {
+	b, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", token)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
+
+func CreateKeyboard(books []models.Book) InlineKeyboardMarkup {
+	buttons := []InlineKeyboardButton{}
+	for i, b := range books {
+		callBack := fmt.Sprintf("%d", b.ID)
+		buttons = append(buttons, InlineKeyboardButton{
+			Text:         fmt.Sprintf("%d", i+1),
+			CallbackData: &callBack,
+		})
+	}
+	return NewInlineKeyboardMarkup(buttons)
+
+}
+
+func TelegramBooksList(user models.User, filters models.BookFilters) (BaseChat, error) {
+	m := NewBaseChat(int64(user.TelegramID), "")
+	booksTxt := []string{}
+	books, _, _ := database.GetBooks(user.ID, filters)
+	for i, b := range books {
+		authors := []string{}
+		for _, a := range b.Authors {
+			authors = append(authors, a.FullName)
+		}
+		booksTxt = append(booksTxt, fmt.Sprintf(`%d. <b>Название:</b> %s
+<b>Автор:</b> %s
+`, i+1, b.Title, strings.Join(authors, ", ")))
+	}
+	m.Text = strings.Join(booksTxt, "\n")
+	m.ReplyMarkup = CreateKeyboard(books)
+	return m, nil
+}
