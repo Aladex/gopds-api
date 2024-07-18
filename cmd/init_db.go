74761e6bbc465028fcf4ceb65d429e06a559e180
Andrey Aleksandrov <aaleksandrov@cinemo.com>
2024-07-18 16:31:56 +0200

    Refactor database and session initialization into dedicated functions.

---
+++ cmd/init_db.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/go-pg/pg/v10"
+	"github.com/go-redis/redis"
+	"github.com/spf13/viper"
+	"gopds-api/sessions"
+	"log"
+)
+
+func initializeDatabase() *pg.DB {
+	options := &pg.Options{
+		User:     viper.GetString("postgres.dbuser"),
+		Password: viper.GetString("postgres.dbpass"),
+		Database: viper.GetString("postgres.dbname"),
+		Addr:     viper.GetString("postgres.dbhost"),
+	}
+	db := pg.Connect(options)
+	if _, err := db.Exec("SELECT 1"); err != nil {
+		log.Fatalln("Failed to connect to database:", err)
+	}
+	return db
+}
+
+func closeDatabaseConnection(db *pg.DB) {
+	if err := db.Close(); err != nil {
+		log.Println("Error closing database connection:", err)
+	}
+}
+
+func initializeSessionManagement() (*redis.Client, *redis.Client) {
+	mainRedisClient := sessions.RedisConnection(0)
+	tokenRedisClient := sessions.RedisConnection(1)
+	return mainRedisClient, tokenRedisClient
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg/v10"
 	"github.com/spf13/viper"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -105,29 +104,11 @@ func corsOptionsMiddleware() gin.HandlerFunc {
 // It sets the gin mode based on the application configuration, ensures the user path exists,
 // sets up middleware, routes, and starts the HTTP server.
 func main() {
-	options := &pg.Options{
-		User:     viper.GetString("postgres.dbuser"),
-		Password: viper.GetString("postgres.dbpass"),
-		Database: viper.GetString("postgres.dbname"),
-		Addr:     viper.GetString("postgres.dbhost"),
-	}
-	db := pg.Connect(options)
-	if _, err := db.Exec("SELECT 1"); err != nil {
-		log.Fatalln("Failed to connect to database:", err)
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Println("Error closing database connection")
-		}
-	}()
-
+	db := initializeDatabase()
+	defer closeDatabaseConnection(db)
 	database.SetDB(db)
 
-	// Initialize Redis connections for session management.
-	mainRedisClient := sessions.RedisConnection(0)
-	// Use a separate Redis database for token storage.
-	tokenRedisClient := sessions.RedisConnection(1)
-	// Set the Redis connections for the session manager
+	mainRedisClient, tokenRedisClient := initializeSessionManagement()
 	sessions.SetRedisConnections(mainRedisClient, tokenRedisClient)
 
 	// Set the Gin mode based on the application configuration.
