fa4a72a7fe09532958cfb36ddac66c9db3a1b0a2
aaleksandrov <aladex@gmail.com>
2024-07-22 10:02:17 +0200

    Add routes for searching books by title and author name with localization.

--- booksdump-frontend/src/routes/privateRoutes.tsx
+++ booksdump-frontend/src/routes/privateRoutes.tsx
@@ -10,6 +10,8 @@ const privateRoutes = (
         <Route path="/books/page/:page" element={<PrivateRoute element={<TitleSetter titleKey="routeTitles.books"><BooksList /></TitleSetter>} />} />
         <Route path="/books/find/author/:id/:page" element={<PrivateRoute element={<TitleSetter titleKey="routeByAuthor.books"><BooksList /></TitleSetter>} />} />
         <Route path="/books/find/category/:id/:page" element={<PrivateRoute element={<TitleSetter titleKey="routeByCategory.books"><BooksList /></TitleSetter>} />} />
+        <Route path="/books/find/books/:title/:page" element={<PrivateRoute element={<TitleSetter titleKey="routeByBookName.books"><BooksList /></TitleSetter>} />} />
+        <Route path="/books/authors/:author/:page" element={<PrivateRoute element={<TitleSetter titleKey="routeByAuthorName.books"><BooksList /></TitleSetter>} />} />
     </>
 );
 
