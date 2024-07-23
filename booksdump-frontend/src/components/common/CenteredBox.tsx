1eaa717eb88f9b2ffbd1388c63156edcc2636359
aaleksandrov <aladex@gmail.com>
2024-07-23 13:59:14 +0200

    Add responsive drawer menu in Header for better mobile navigation.

--- booksdump-frontend/src/components/common/CenteredBox.tsx
+++ booksdump-frontend/src/components/common/CenteredBox.tsx
@@ -1,6 +1,7 @@
 // src/components/common/CenteredBox.tsx
 import React from 'react';
 import { Box, Card } from '@mui/material';
+import theme from '../../theme';
 
 interface ILoginCenteredBoxProps {
     children: React.ReactNode;
@@ -9,8 +10,8 @@ interface ILoginCenteredBoxProps {
 const LoginCenteredBox: React.FC<ILoginCenteredBoxProps> = ({ children }) => {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-            <Card sx={{ minWidth: 700 }}>
-                {children}
+            <Card sx={{ padding: 3, [theme.breakpoints.up('md')]: { minWidth: 700 } }}>
+            {children}
             </Card>
         </Box>
     );
--- booksdump-frontend/src/components/common/Header.tsx
+++ booksdump-frontend/src/components/common/Header.tsx
@@ -1,26 +1,41 @@
-// src/components/Header.tsx
-import React, { useState, useEffect } from 'react';
-import {AppBar, Toolbar, Typography, Button, Tabs, Tab, Box, IconButton} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
-import { useTheme } from '@mui/material/styles';
+import React, {useState, useEffect} from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Tabs,
+    Tab,
+    Box,
+    IconButton,
+    Drawer,
+    List,
+    ListItemText,
+    ListItemButton
+} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {useAuth} from '../../context/AuthContext';
+import {useTheme} from '@mui/material/styles';
+import {useMediaQuery} from '@mui/material';
 import axios from 'axios';
-import { API_URL } from '../../api/config';
+import {API_URL} from '../../api/config';
 import {useTranslation} from 'react-i18next';
-import {Logout} from "@mui/icons-material";
+import {Menu as MenuIcon, Logout, Person} from "@mui/icons-material";
 
 const Header: React.FC = () => {
-    const { logout, token, updateUser, user } = useAuth();
+    const {logout, token, updateUser, user} = useAuth();
     const navigate = useNavigate();
     const theme = useTheme();
     const {t} = useTranslation();
     const [value, setValue] = useState(0);
+    const isMobile = useMediaQuery('(max-width:600px)');
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const response = await axios.get(`${API_URL}/books/self-user`, {
-                    headers: { Authorization: `${token}` },
+                    headers: {Authorization: `${token}`},
                 });
                 updateUser(response.data); // Update user data in context
             } catch (error) {
@@ -42,7 +57,7 @@ const Header: React.FC = () => {
 
     const handleUserInfo = () => {
         // Add user info dialog window
-    }
+    };
 
     const a11yProps = (index: number) => {
         return {
@@ -51,28 +66,97 @@ const Header: React.FC = () => {
         };
     };
 
+    const handleDrawerToggle = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    const menuItems = [
+        {label: t('booksTab'), path: '/books/page/1', index: 0},
+        {label: t('opdsTab'), path: '/catalog', index: 1},
+        {label: t('donateTab'), path: '/donate', index: 2},
+    ];
+
+    if (user?.is_superuser) {
+        menuItems.push({label: t('adminTab'), path: '/admin', index: 3});
+    }
+
     return (
-        <AppBar position="static" sx={{ bgcolor: theme.palette.secondary.main }}>
+        <AppBar position="static" sx={{bgcolor: theme.palette.secondary.main}}>
             <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1, color: theme.palette.primary.main }}>
-                    BOOKSDUMP
-                </Typography>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label={t('booksTab')} {...a11yProps(0)} onClick={() => navigate('/books/page/1')} sx={{ color: value === 0 ? 'inherit' : '#9e9e9e' }} />
-                        <Tab label={t('opdsTab')} {...a11yProps(1)} onClick={() => navigate('/catalog')} sx={{ color: value === 1 ? 'inherit' : '#9e9e9e' }} />
-                        <Tab label={t('donateTab')} {...a11yProps(2)} onClick={() => navigate('/donate')} sx={{ color: value === 2 ? 'inherit' : '#9e9e9e' }} />
-                        {user?.is_superuser && <Tab label={t('adminTab')} {...a11yProps(3)} onClick={() => navigate('/admin')} sx={{ color: value === 3 ? 'inherit' : '#9e9e9e' }} />}
-                    </Tabs>
-                </Box>
-                <Button sx={{ color: theme.palette.primary.main }} onClick={handleUserInfo}>
-                    {user?.username}
-                </Button>
-                <Button sx={{ color: theme.palette.primary.main }} onClick={handleLogout}>
-                    <IconButton color="inherit">
-                        <Logout />
-                    </IconButton>
-                </Button>
+                {isMobile ? (
+                    <>
+                        <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
+                            <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
+                                <Box display="flex" alignItems="center">
+                                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerToggle}>
+                                        <MenuIcon color="primary"/>
+                                    </IconButton>
+                                </Box>
+                                <Box display="flex" alignItems="center">
+                                    <Button sx={{color: theme.palette.primary.main}} onClick={handleUserInfo}>
+                                        <Person />
+                                    </Button>
+                                    <Button sx={{color: theme.palette.primary.main}} onClick={handleLogout}>
+                                        <Logout/>
+                                    </Button>
+                                </Box>
+                            </Box>
+                            <Drawer
+                                anchor="left"
+                                open={drawerOpen}
+                                onClose={handleDrawerToggle}
+                                ModalProps={{
+                                    keepMounted: true, // Better open performance on mobile.
+                                }}
+                                PaperProps={{
+                                    sx: {
+                                        width: '50%', // Occupy the full width of the screen
+                                    }
+                                }}
+                            >
+                                <List>
+                                    {menuItems.map((item, index) => (
+                                        <ListItemButton key={index} onClick={() => {
+                                            navigate(item.path);
+                                            handleDrawerToggle();
+                                        }}>
+                                            <ListItemText primary={item.label.toUpperCase()}/>
+                                        </ListItemButton>
+                                    ))}
+                                    <ListItemButton onClick={handleUserInfo}>
+                                        <ListItemText primary={user?.username}/>
+                                    </ListItemButton>
+                                </List>
+                            </Drawer></Box>
+                    </>
+                ) : (
+                    <>
+                        <Typography variant="h6" sx={{flexGrow: 1, color: theme.palette.primary.main}}>
+                            BOOKSDUMP
+                        </Typography>
+                        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                                {menuItems.map((item, index) => (
+                                    <Tab
+                                        key={index}
+                                        label={item.label}
+                                        {...a11yProps(item.index)}
+                                        onClick={() => navigate(item.path)}
+                                        sx={{color: value === item.index ? 'inherit' : '#9e9e9e'}}
+                                    />
+                                ))}
+                            </Tabs>
+                        </Box>
+                        <Button sx={{color: theme.palette.primary.main}} onClick={handleUserInfo}>
+                            {user?.username}
+                        </Button>
+                        <Button sx={{color: theme.palette.primary.main}} onClick={handleLogout}>
+                            <IconButton color="inherit">
+                                <Logout/>
+                            </IconButton>
+                        </Button>
+                    </>
+                )}
             </Toolbar>
         </AppBar>
     );
