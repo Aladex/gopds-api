bba8f58b435871fc26061bfbcee73c2e4109f2c1
aaleksandrov <aladex@gmail.com>
2024-07-25 16:07:39 +0200

    Make image size responsive for mobile devices in CenteredBox component.

--- booksdump-frontend/src/components/common/CenteredBox.tsx
+++ booksdump-frontend/src/components/common/CenteredBox.tsx
@@ -1,6 +1,5 @@
-// src/components/common/CenteredBox.tsx
 import React from 'react';
-import { Box, Card } from '@mui/material';
+import { Box, Card, useMediaQuery } from '@mui/material';
 import theme from '../../theme';
 import { Link } from 'react-router-dom';
 
@@ -9,13 +8,24 @@ interface ILoginCenteredBoxProps {
 }
 
 const LoginCenteredBox: React.FC<ILoginCenteredBoxProps> = ({ children }) => {
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
         <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
             <Card sx={{ padding: 3, [theme.breakpoints.up('md')]: { minWidth: 700 } }}>
-            {children}
+                {children}
             </Card>
             <Link to="/registration" style={{ position: 'absolute', bottom: 0, right: 0 }}>
-                <img src="/devito_back.png" alt="Registration" style={{ width: 378, height: 256, display: 'block', border: 'none' }} />
+                <img
+                    src="/devito_back.png"
+                    alt="Registration"
+                    style={{
+                        width: isMobile ? '100px' : '378px',
+                        height: isMobile ? '68px' : '256px',
+                        display: 'block',
+                        border: 'none'
+                    }}
+                />
             </Link>
         </Box>
     );
