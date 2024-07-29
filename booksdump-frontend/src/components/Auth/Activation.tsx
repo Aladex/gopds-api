2590076b8bf758e39b6908a7777f489ecf65811b
aaleksandrov <aladex@gmail.com>
2024-07-29 07:37:11 +0200

    Add account activation feature with new route, component, and translations.

---
+++ booksdump-frontend/src/components/Auth/Activation.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { API_URL } from '../../api/config';
+import { useTranslation } from 'react-i18next';
+import { LinearProgress, Box, Typography } from '@mui/material';
+import LoginCenteredBox from '../common/CenteredBox';
+import CardContent from '@mui/material/CardContent';
+
+const Activation: React.FC = () => {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+    const { token } = useParams<{ token: string }>();
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const tokenValidation = async () => {
+            try {
+                const response = await fetch(`${API_URL}/api/change-password`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ token }),
+                });
+
+                if (response.status === 404) {
+                    navigate('/404');
+                } else {
+                    setTimeout(() => {
+                        navigate('/login');
+                    }, 5000);
+                }
+            } catch {
+                navigate('/404');
+            }
+        };
+        tokenValidation();
+    }, [token, navigate]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((oldProgress: number) => {
+                if (oldProgress === 100) {
+                    clearInterval(timer);
+                    return 100;
+                }
+                return Math.min(oldProgress + 2, 100);
+            });
+        }, 100);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return (
+        <LoginCenteredBox>
+            <CardContent>
+                <Typography variant="h6" textAlign="center">{t('activation')}</Typography>
+                <Box textAlign="center">
+                    <Typography>{t('activationSuccess')}</Typography>
+                    <LinearProgress variant="determinate" value={progress} color={"secondary"} />
+                </Box>
+            </CardContent>
+        </LoginCenteredBox>
+    );
+};
+
+export default Activation;
--- booksdump-frontend/src/routes/publicRoutes.tsx
+++ booksdump-frontend/src/routes/publicRoutes.tsx
@@ -7,6 +7,7 @@ import ForgotPassword from '../components/Auth/ForgotPassword';
 import TitleSetter from '../components/common/TitleSetter';
 import PublicRoute from "../components/PublicRoute";
 import ChangePassword from "../components/Auth/ChangePassword";
+import Activation from "../components/Auth/Activation";
 
 const publicRoutes = (
     <Route path="/" element={<PublicRoute />}>
@@ -14,6 +15,7 @@ const publicRoutes = (
         <Route path="/registration" element={<TitleSetter titleKey="routeTitles.registration"><Registration /></TitleSetter>} />
         <Route path="/forgot-password" element={<TitleSetter titleKey="routeTitles.forgotPassword"><ForgotPassword /></TitleSetter>} />
         <Route path="/change-password/:token" element={<TitleSetter titleKey="routeTitles.forgotPassword"><ChangePassword /></TitleSetter>} />
+        <Route path="/activation/:token" element={<TitleSetter titleKey="routeTitles.activation"><Activation /></TitleSetter>} />
     </Route>
 );
 
