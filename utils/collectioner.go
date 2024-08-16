00b5dbf6c8448f02c464645303a6fe51e250dd2f
Andrey Aleksandrov <aaleksandrov@cinemo.com>
2024-08-16 14:47:34 +0200

    Support multiple formats in collection management functions.

--- utils/collectioner.go
+++ utils/collectioner.go
@@ -14,39 +14,46 @@ type CollectionManager struct {
 }
 
 // AddBookToCollection adds a book to the collection
-func (cm *CollectionManager) AddBookToCollection(bookID, position int, format string, reader io.Reader) error {
+func (cm *CollectionManager) AddBookToCollection(bookID, position int, reader io.Reader) error {
 	collectionPath := filepath.Join(cm.BasePath, fmt.Sprintf("collection_%d", cm.CollectionID))
 	if err := os.MkdirAll(collectionPath, 0755); err != nil {
 		return fmt.Errorf("failed to create collection directory: %w", err)
 	}
 
-	fileName := fmt.Sprintf("%02d_%d.%s", position, bookID, format)
-	filePath := filepath.Join(collectionPath, fileName)
+	formats := []string{"fb2", "epub", "mobi"}
+	for _, format := range formats {
+		fileName := fmt.Sprintf("%02d_%d.%s", position, bookID, format)
+		filePath := filepath.Join(collectionPath, fileName)
 
-	file, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
-	}
-	defer file.Close()
+		file, err := os.Create(filePath)
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+		defer file.Close()
 
-	if _, err := io.Copy(file, reader); err != nil {
-		return fmt.Errorf("failed to write file: %w", err)
+		if _, err := io.Copy(file, reader); err != nil {
+			return fmt.Errorf("failed to write file: %w", err)
+		}
 	}
 
 	return nil
 }
 
 // RemoveBookFromCollection removes a book from the collection
-func (cm *CollectionManager) RemoveBookFromCollection(bookID int, position int, format string) error {
+func (cm *CollectionManager) RemoveBookFromCollection(bookID int, position int) error {
 	collectionPath := filepath.Join(cm.BasePath, fmt.Sprintf("collection_%d", cm.CollectionID))
-	fileName := fmt.Sprintf("%02d_%d.%s", position, bookID, format)
-	filePath := filepath.Join(collectionPath, fileName)
+	formats := []string{"fb2", "epub", "mobi"}
 
-	if err := os.Remove(filePath); err != nil {
-		if os.IsNotExist(err) {
-			return nil // ignore if file does not exist
+	for _, format := range formats {
+		fileName := fmt.Sprintf("%02d_%d.%s", position, bookID, format)
+		filePath := filepath.Join(collectionPath, fileName)
+
+		if err := os.Remove(filePath); err != nil {
+			if os.IsNotExist(err) {
+				continue // ignore if file does not exist
+			}
+			return fmt.Errorf("failed to remove file: %w", err)
 		}
-		return fmt.Errorf("failed to remove file: %w", err)
 	}
 
 	return nil
@@ -55,29 +62,32 @@ func (cm *CollectionManager) RemoveBookFromCollection(bookID int, position int, 
 // RenumberBooksInCollection updates the positions of books in the collection
 func (cm *CollectionManager) RenumberBooksInCollection(newPositions map[int]int) error {
 	collectionPath := filepath.Join(cm.BasePath, fmt.Sprintf("collection_%d", cm.CollectionID))
+	formats := []string{"fb2", "epub", "mobi"}
 
 	for oldPosition, newPosition := range newPositions {
-		oldFileName := fmt.Sprintf("%02d_", oldPosition)
-		newFileName := fmt.Sprintf("%02d_", newPosition)
+		for _, format := range formats {
+			oldFileName := fmt.Sprintf("%02d_", oldPosition)
+			newFileName := fmt.Sprintf("%02d_", newPosition)
 
-		err := filepath.Walk(collectionPath, func(path string, info os.FileInfo, err error) error {
+			err := filepath.Walk(collectionPath, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				// If the file name starts with the old position, rename it to the new position
+				if filepath.Base(path)[:len(oldFileName)] == oldFileName && filepath.Ext(path) == "."+format {
+					newPath := filepath.Join(collectionPath, newFileName+filepath.Base(path)[len(oldFileName):])
+					if err := os.Rename(path, newPath); err != nil {
+						return fmt.Errorf("failed to rename file %s to %s: %w", path, newPath, err)
+					}
+				}
+
+				return nil
+			})
+
 			if err != nil {
 				return err
 			}
-
-			// If the file name starts with the old position, rename it to the new position
-			if filepath.Base(path)[:len(oldFileName)] == oldFileName {
-				newPath := filepath.Join(collectionPath, newFileName+filepath.Base(path)[len(oldFileName):])
-				if err := os.Rename(path, newPath); err != nil {
-					return fmt.Errorf("failed to rename file %s to %s: %w", path, newPath, err)
-				}
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			return err
 		}
 	}
 
