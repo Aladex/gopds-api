6cde299710d6892fa01a626a776a8cb2fc9b2f43
Andrei Aleksandrov <aaleksandrov@cinemo.com>
2024-10-30 18:17:25 +0100

    Added file checking in ws

--- api/websocket.go
+++ api/websocket.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gobwas/ws"
@@ -9,10 +10,12 @@ import (
 	"github.com/spf13/viper"
 	"gopds-api/database"
 	"gopds-api/httputil"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 )
 
 // deleteFile deletes the file at the specified path
@@ -64,19 +67,6 @@ func DownloadConvertedBook(c *gin.Context) {
 
 var notificationChannel = make(chan string)
 
-func notifyClientBookReady(bookID int64) {
-	logrus.Infof("Notifying client that book %d is ready", bookID)
-	if ch, ok := readyChannels.Load(bookID); ok {
-		// Close the channel to notify the client that the book is ready
-		close(ch.(chan struct{}))
-		readyChannels.Delete(bookID)
-		notificationChannel <- fmt.Sprintf("%d", bookID) // Send the book ID to the notification channel
-		logrus.Infof("Book %d notification sent to notificationChannel", bookID)
-	} else {
-		logrus.Warnf("Notification channel for book %d not found in readyChannels", bookID)
-	}
-}
-
 func WebsocketHandler(c *gin.Context) {
 	conn, _, _, err := ws.UpgradeHTTP(c.Request, c.Writer)
 	if err != nil {
@@ -88,19 +78,40 @@ func WebsocketHandler(c *gin.Context) {
 	quit := make(chan struct{})
 
 	go func() {
-		// Read from the WebSocket connection to keep it alive
+		// Чтение сообщений от клиента
 		for {
-			if _, _, err := wsutil.ReadClientData(conn); err != nil {
+			msg, op, err := wsutil.ReadClientData(conn)
+			if err != nil {
 				logrus.Warn("WebSocket connection closed by client.")
 				close(quit)
 				return
+			}
+
+			// Логируем сообщение от клиента
+			if op == ws.OpText {
+				logrus.Infof("Received message from client: %s", string(msg))
+
+				// Парсим `bookID` из сообщения клиента
+				var request struct {
+					BookID int64  `json:"bookID"`
+					Format string `json:"format"`
+				}
+				if err := json.Unmarshal(msg, &request); err != nil {
+					logrus.Error("Failed to parse message from client:", err)
+					continue
+				}
+
+				// Запускаем проверку файла книги
+				go checkFileAndNotify(conn, request.BookID)
+			} else {
+				logrus.Infof("Received non-text message from client with opcode: %v", op)
 			}
 		}
 	}()
 
 	for {
 		select {
-		case bookID := <-notificationChannel: // Wait for a book to be ready
+		case bookID := <-notificationChannel: // Ожидаем, пока книга будет готова
 			logrus.Infof("Notifying client about ready book: %s", bookID)
 			if err := wsutil.WriteServerMessage(conn, ws.OpText, []byte(bookID)); err != nil {
 				logrus.Warn("Error writing to WebSocket:", err)
@@ -109,6 +120,30 @@ func WebsocketHandler(c *gin.Context) {
 			}
 		case <-quit:
 			logrus.Info("Connection closed by client request.")
+			return
+		}
+	}
+}
+
+// checkFileAndNotify проверяет наличие файла и уведомляет клиента, если файл найден
+func checkFileAndNotify(conn net.Conn, bookID int64) {
+	mobiConversionDir := viper.GetString("app.mobi_conversion_dir")
+	filePath := filepath.Join(mobiConversionDir, fmt.Sprintf("%d.mobi", bookID))
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		// Проверяем наличие файла
+		if _, err := os.Stat(filePath); err == nil {
+			// Файл найден, уведомляем клиента и выходим
+			logrus.Infof("Book %d is ready on disk, notifying client", bookID)
+			if err := wsutil.WriteServerMessage(conn, ws.OpText, []byte(fmt.Sprintf("%d", bookID))); err != nil {
+				logrus.Warn("Error writing to WebSocket:", err)
+			}
+			return
+		} else if !os.IsNotExist(err) {
+			logrus.Errorf("Error checking file %s: %v", filePath, err)
 			return
 		}
 	}
